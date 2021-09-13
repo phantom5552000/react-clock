6748aaeb1fa58ace596f9774dd78044b63d4d208
phantom5552000 <phantom5552000@users.noreply.github.com>
2021-09-14 05:59:48 +0900

    index.htmlで動く時計

---
+++ src/Clock.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function formatDate (date, format) {
+    format = format.replace(/yyyy/g, date.getFullYear());
+    format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+    format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
+    format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2));
+    format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+    format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+    format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
+    return format;
+};
+  
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        100
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({         // 直接 stateに代入してはだめ。reactの基本
+        date: new Date()
+      });
+    }  
+  
+    render() {
+      return (
+        <div>
+           {formatDate(this.state.date, 'HH:mm:ss:SSS')}
+  
+        </div>
+      );
+    }
+}
+
+export default  Clock;
